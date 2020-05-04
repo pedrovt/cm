561e7602c551145f2b6a0da37d8a2f7593164988
Pedro Teixeira <pedro.teix@ua.pt>
2020-05-04 01:02:19 +0100

    Added Lab 2, Exercise 3 (Fragment communication)

--- Lab2/2_Fragments/FragmentExample_start/app/src/main/java/com/example/android/fragmentexample/MainActivity.java
+++ Lab2/2_Fragments/FragmentExample_start/app/src/main/java/com/example/android/fragmentexample/MainActivity.java
@@ -22,12 +22,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SimpleFragment.OnFragmentInteractionListener {
 
     private Button mButton;
     private boolean isFragmentDisplayed = false;
     static final String STATE_FRAGMENT = "state_of_fragment";
+    private int mRadioButtonChoice = 2; // The default (no choice).
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,14 +66,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayFragment() {
-        SimpleFragment simpleFragment = SimpleFragment.newInstance();
+        SimpleFragment fragment = SimpleFragment.newInstance(mRadioButtonChoice);
         // Get the FragmentManager and start a transaction.
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager
                 .beginTransaction();
         // Add the SimpleFragment.
         fragmentTransaction.add(R.id.fragment_container,
-                simpleFragment).addToBackStack(null).commit();
+                fragment).addToBackStack(null).commit();
         // Update the Button text.
         mButton.setText(R.string.close);
         // Set boolean flag to indicate fragment is open.
@@ -94,5 +96,13 @@ public class MainActivity extends AppCompatActivity {
         mButton.setText(R.string.open);
         // Set boolean flag to indicate fragment is closed.
         isFragmentDisplayed = false;
+    }
+
+    @Override
+    public void onRadioButtonChoice(int choice) {
+        // Keep the radio button choice to pass it back to the fragment.
+        mRadioButtonChoice = choice;
+        Toast.makeText(this, "Choice is " + Integer.toString(choice),
+                Toast.LENGTH_SHORT).show();
     }
 }
--- Lab2/2_Fragments/FragmentExample_start/app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
+++ Lab2/2_Fragments/FragmentExample_start/app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.fragmentexample;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -16,6 +17,12 @@ public class SimpleFragment extends Fragment {
 
     private static final int YES = 0;
     private static final int NO = 1;
+    private static final int NONE = 2;
+
+    public int mRadioButtonChoice = NONE;
+    OnFragmentInteractionListener mListener;
+
+    private static final String CHOICE = "choice";
 
     public SimpleFragment() {
         // Required empty public constructor
@@ -42,16 +49,31 @@ public class SimpleFragment extends Fragment {
                   switch (index) {
                       case YES: // User chose "Yes."
                           textView.setText(R.string.yes_message);
+                          mRadioButtonChoice = YES;
+                          mListener.onRadioButtonChoice(YES);
                           break;
                       case NO: // User chose "No."
                           textView.setText(R.string.no_message);
+                          mRadioButtonChoice = NO;
+                          mListener.onRadioButtonChoice(NO);
                           break;
                       default: // No choice made.
-                          // Do nothing.
+                          mRadioButtonChoice = NONE;
+                          mListener.onRadioButtonChoice(NONE);
                           break;
                   }
               }
           });
+
+        if (getArguments().containsKey(CHOICE)) {
+            // A choice was made, so get the choice.
+            mRadioButtonChoice = getArguments().getInt(CHOICE);
+            // Check the radio button choice.
+            if (mRadioButtonChoice != NONE) {
+                radioGroup.check
+                        (radioGroup.getChildAt(mRadioButtonChoice).getId());
+            }
+        }
 
         // Return the View for the fragment's UI.
         return rootView;
@@ -59,5 +81,29 @@ public class SimpleFragment extends Fragment {
 
     public static SimpleFragment newInstance() {
         return new SimpleFragment();
+    }
+
+    public static SimpleFragment newInstance(int choice) {
+        SimpleFragment fragment = new SimpleFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(CHOICE, choice);
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new ClassCastException(context.toString()
+                    + getResources().getString(R.string.exception_message));
+        }
+    }
+
+
+    interface OnFragmentInteractionListener {
+        void onRadioButtonChoice(int choice);
     }
 }
