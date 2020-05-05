d947c2438eebc9f76462653d8fbfe459750975b7
Pedro Teixeira <pedro.teix@ua.pt>
2020-05-05 14:15:13 +0100

    Homework 2, given code

---
+++ Homeworks/2_NextWeather/app/src/main/java/pt/ua/nextweather/ui/MainActivity.java
@@ -0,0 +1,137 @@
+package pt.ua.nextweather.ui;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+import pt.ua.nextweather.R;
+import pt.ua.nextweather.datamodel.City;
+import pt.ua.nextweather.datamodel.Weather;
+import pt.ua.nextweather.datamodel.WeatherType;
+import pt.ua.nextweather.network.CityResultsObserver;
+import pt.ua.nextweather.network.ForecastForACityResultsObserver;
+import pt.ua.nextweather.network.IpmaWeatherClient;
+import pt.ua.nextweather.network.WeatherTypesResultsObserver;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView feedback;
+
+    IpmaWeatherClient client = new IpmaWeatherClient();
+    private HashMap<String, City> cities;
+    private HashMap<Integer, WeatherType> weatherDescriptions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callWeatherForecastForACityStep1("Aveiro");
+            }
+        });
+
+        feedback = findViewById(R.id.tvFeedback);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void callWeatherForecastForACityStep1(String city) {
+
+        feedback.append("\nGetting forecast for: " + city); feedback.append("\n");
+
+        // call the remote api, passing an (anonymous) listener to get back the results
+        client.retrieveWeatherConditionsDescriptions(new WeatherTypesResultsObserver() {
+            @Override
+            public void receiveWeatherTypesList(HashMap<Integer, WeatherType> descriptorsCollection) {
+                MainActivity.this.weatherDescriptions = descriptorsCollection;
+                callWeatherForecastForACityStep2( city);
+            }
+            @Override
+            public void onFailure(Throwable cause) {
+                feedback.append("Failed to get weather conditions!" + cause.getMessage());
+            }
+        });
+
+    }
+
+    private void callWeatherForecastForACityStep2(String city) {
+        client.retrieveCitiesList(new CityResultsObserver() {
+
+            @Override
+            public void receiveCitiesList(HashMap<String, City> citiesCollection) {
+                MainActivity.this.cities = citiesCollection;
+                City cityFound = cities.get(city);
+                if( null != cityFound) {
+                    int locationId = cityFound.getGlobalIdLocal();
+                    callWeatherForecastForACityStep3(locationId);
+                } else {
+                    feedback.append("unknown city: " + city);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable cause) {
+                feedback.append("Failed to get cities list!");
+            }
+        });
+    }
+
+    private void callWeatherForecastForACityStep3(int localId) {
+        client.retrieveForecastForCity(localId, new ForecastForACityResultsObserver() {
+            @Override
+            public void receiveForecastList(List<Weather> forecast) {
+                for (Weather day : forecast) {
+                    feedback.append(day.toString());
+                    feedback.append("\t");
+                }
+            }
+            @Override
+            public void onFailure(Throwable cause) {
+                feedback.append( "Failed to get forecast for 5 days");
+            }
+        });
+
+    }
+
+}
