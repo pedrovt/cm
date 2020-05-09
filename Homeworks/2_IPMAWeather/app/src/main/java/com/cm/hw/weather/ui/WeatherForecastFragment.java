efd2ceb986939eae669a313a7c9124806c996d16
Pedro Teixeira <pedro.teix@ua.pt>
2020-05-09 22:38:08 +0100

    Homework 2, Added recycler view for several days of forecast

--- Homeworks/2_IPMAWeather/app/src/main/java/com/cm/hw/weather/ui/WeatherForecastFragment.java
+++ Homeworks/2_IPMAWeather/app/src/main/java/com/cm/hw/weather/ui/WeatherForecastFragment.java
@@ -5,18 +5,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.cm.hw.weather.R;
 import com.cm.hw.weather.datamodel.Weather;
 import com.cm.hw.weather.datamodel.WeatherType;
-import com.cm.hw.weather.datamodel.WindSpeed;
 import com.google.gson.Gson;
 
 import java.util.List;
-import java.util.Map;
 
 public class WeatherForecastFragment extends Fragment {
 
@@ -55,45 +56,20 @@ public class WeatherForecastFragment extends Fragment {
      * @return Fragment view
      */
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.weather_forecast_fragment,
                 container, false);
 
-        // Show the detail information in a TextView.
+        // Show the detail information in a Recycler Viw.
         if (mForecast != null) {
             for (int i = 0; i < mForecast.length; i++) {
                 Log.i("Forecast Info for day " + (i + 1), mForecast[i].toString());
             }
-            // TODO other days
-            Weather forecast = mForecast[0];
-
-            // City Name
-            ((TextView) rootView.findViewById(R.id.city_name)).setText(forecast.getCityName());
-
-            // Forecast Date
-            ((TextView) rootView.findViewById(R.id.forecast_date)).setText(forecast.getForecastDate());
-
-            // Weather Type (icon + string)
-            // TODO icon
-            WeatherType weatherType = MainActivity.weatherDescriptions.get(forecast.getIdWeatherType());
-            ((TextView) rootView.findViewById(R.id.status)).setText(weatherType.getDescIdWeatherTypeEN());
-
-            // Minimum Temperature
-            ((TextView) rootView.findViewById(R.id.min_temp)).setText(String.format("%s ºC", forecast.getTMin()));
-
-            // Maximum Temperature
-            ((TextView) rootView.findViewById(R.id.max_temp)).setText(String.format("%s ºC", forecast.getTMax()));
-
-            // Wind (direction + speed)
-            String windSpeed = MainActivity.windDescriptions.get(forecast.getClassWindSpeed());
-            ((TextView) rootView.findViewById(R.id.wind)).setText(String.format("%s - %s", windSpeed, forecast.getPredWindDir()));
-
-            // Rain Probability
-            ((TextView) rootView.findViewById(R.id.rain_probability)).setText(String.format("%s %%", forecast.getPrecipitaProb()));
-
         }
 
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.weather_forecast_details_container);
+        recyclerView.setAdapter(new ForecastRecyclerViewAdapter(mForecast));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
         return rootView;
     }
 
@@ -116,5 +92,93 @@ public class WeatherForecastFragment extends Fragment {
         arguments.putString(FORECAST_KEY, forecastJsonList);
         fragment.setArguments(arguments);
         return fragment;
+    }
+
+    /**
+     * The RecyclerView for the forecast
+     */
+    class ForecastRecyclerViewAdapter extends RecyclerView.Adapter<ForecastRecyclerViewAdapter.ForecastViewHolder> {
+        private final Weather[] mValues;
+
+        ForecastRecyclerViewAdapter(Weather[] items) {
+            mValues = items;
+        }
+
+        /**
+         * This method inflates the layout for the cities list.
+         * @param parent ViewGroup into which the new view will be added.
+         * @param viewType The view type of the new View.
+         * @return A new ViewHolder
+         */
+        @Override
+        public ForecastViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.weather_forecast, parent, false);
+            return new ForecastViewHolder(view);
+        }
+
+        /**
+         * @param holder   ViewHolder
+         * @param position Position of the city in the array.
+         */
+        @Override
+        public void onBindViewHolder(final ForecastViewHolder holder, int position) {
+            Weather forecast = mValues[position];
+
+            // City Name
+            holder.mCityName.setText(forecast.getCityName());
+
+            // Forecast Date
+            holder.mForecastDate.setText(forecast.getForecastDate());
+
+            // Weather Type (icon + string)
+            WeatherType weatherType = MainActivity.weatherDescriptions.get(forecast.getIdWeatherType());
+            holder.mStatus.setText(weatherType.getDescIdWeatherTypeEN());
+            holder.mStatusIcon.setImageResource(R.drawable.weather_state_cloudy);       // TODO
+
+            // Minimum Temperature
+            holder.mMinTemp.setText(String.format("%s ºC", forecast.getTMin()));
+
+            // Maximum Temperature
+            holder.mMaxTemp.setText(String.format("%s ºC", forecast.getTMax()));
+
+            // Wind (direction + speed)
+            String windSpeed = MainActivity.windDescriptions.get(forecast.getClassWindSpeed());
+            holder.mWind.setText(String.format("%s - %s", windSpeed, forecast.getPredWindDir()));
+
+            // Rain Probability
+            holder.mRainProb.setText(String.format("%s %%", forecast.getPrecipitaProb()));
+        }
+
+        /**
+         * Get the count of forecast items.
+         * @return Integer count
+         */
+        @Override
+        public int getItemCount() {
+            return mValues.length;
+        }
+
+        /**
+         * ViewHolder describes an item view and metadata about its place
+         * within the RecyclerView.
+         */
+        class ForecastViewHolder extends RecyclerView.ViewHolder {
+            final View mView;
+            final TextView mCityName, mForecastDate, mStatus, mMinTemp, mMaxTemp, mWind, mRainProb;
+            final ImageView mStatusIcon;
+
+            ForecastViewHolder(View view) {
+                super(view);
+                mView = view;
+                mCityName     = (TextView)  view.findViewById(R.id.city_name);
+                mForecastDate = (TextView)  view.findViewById(R.id.forecast_date);
+                mStatus       = (TextView)  view.findViewById(R.id.status);
+                mStatusIcon   = (ImageView) view.findViewById(R.id.status_icon);
+                mMinTemp      = (TextView)  view.findViewById(R.id.min_temp);
+                mMaxTemp      = (TextView)  view.findViewById(R.id.max_temp);
+                mWind         = (TextView)  view.findViewById(R.id.wind);
+                mRainProb     = (TextView)  view.findViewById(R.id.rain_probability);
+            }
+        }
     }
 }
